93ef46e98d90519a70906426b8801b7e11a24af1
Leo <leo@leocrawford.org.uk>
2015-05-30 19:29:46 +0100

    touch v to 0.93

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='picasawebsync',
-      version='0.92',
+      version='0.93',
       py_modules=['picasawebsync'],
       scripts=['picasawebsync.py']
       )
